ce5fd0b33a4b2d2efe5c7cdbc4dd63d8c3e56000
riyyancmic@gmail.com <riyyancmic@gmail.com>
2024-05-02 20:43:59 +0700

    230441100164_MRiyyanKusumaZunaedi=kakCindy_Modul4

---
+++ modul-4/230441100164_M RIYYAN KUSUMA ZUNAEDI/dropdown/dropdown2.js
@@ -0,0 +1,10 @@
+let submenu=document.getElementById("submenu");
+
+        function toggleMenu(){
+            submenu.classList.toggle("openMenu");
+        }
+        feather.replace();
+
+            function beliClicked() {
+            alert('hp telah dibeli!');
+        }

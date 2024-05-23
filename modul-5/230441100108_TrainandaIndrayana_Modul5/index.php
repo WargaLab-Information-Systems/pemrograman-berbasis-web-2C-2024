268f2bc955bb02f8373d246c3de3182025956af5
trinanda537 <trinandaindrayana7@gmail.com>
2024-05-23 18:38:03 +0700

    230441100108_TrinandaIndrayana_DeviDwiNovitasari_modul5

---
+++ modul-5/230441100108_TrainandaIndrayana_Modul5/index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+if(isset($_SESSION['username'])) {
+    header("Location: home.php");
+    exit;
+}
+
+if(isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    
+    $_SESSION['username'] = $username;
+    header("Location: home.php");
+    exit;
+}
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Login</h2>
+        <?php if(isset($error)) { ?>
+            <div class="error"><?php echo $error; ?></div>
+        <?php } ?>
+        <form action="" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <button type="submit" name="login">Login</button>
+        </form>
+    </div>
+</body>
+</html>
